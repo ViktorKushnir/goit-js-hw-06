3f73cce4d5d423048c54537c75dc10bb9f557b91
ViktorKushnir <kushnirvv111@gmail.com>
2023-04-13 00:10:00 +0100

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const elUl = document.querySelector("#ingredients");
+for (const ingredient of ingredients) {
+  const elLi = document.createElement("li");
+  elLi.textContent = ingredient;
+  elLi.classList.add("item");
+  elUl.appendChild(elLi);
+}
